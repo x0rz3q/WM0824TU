4ac635c748319f9707ad79029dd480346ac3899e
Marco <hello.marcopalazzo@gmail.com>
2020-09-18 22:06:11 +0200

    proper timeseries

--- src/category_lifetime.py
+++ src/category_lifetime.py
@@ -3,31 +3,20 @@ import matplotlib.pyplot as plt
 from functools import reduce
 
 # TODO: make plot less ugly
-# TODO: deal with timeframes extending over multiple years
 class CategoryLifetime:
 
     @staticmethod
     def render(df):
-        df = df[['category', 'first_observed', 'last_observed']] # only use relevant columns
+        df = df[['category', 'date', 'first_observed', 'last_observed']] # only use relevant columns
         df['first_observed'] = pd.to_datetime(df['first_observed']) # turn dates into datetime objects
         df['last_observed'] = pd.to_datetime(df['last_observed'])
-        first_year = min(df['first_observed']).year
-        last_year = max(df['last_observed']).year
         df['product_lifetime'] = df['last_observed'] - df['first_observed']
+        df['period'] = pd.DatetimeIndex(df['date']).to_period('Y')
 
-        dfs = []
-        for i in range(first_year, last_year+1):
-            df_by_year = df[(df['first_observed'] >= pd.Timestamp(str(i))) & (df['last_observed'] < pd.Timestamp(str(i+1)))]
-            df_by_year_mean = df_by_year[['category', 'product_lifetime']].groupby('category').mean(numeric_only=False)
-            df_by_year_mean['product_lifetime'] = df_by_year_mean['product_lifetime'].dt.days
-            df_by_year_mean = df_by_year_mean.rename(columns={'product_lifetime': f'{str(i)}'})
-            dfs.append(df_by_year_mean)
+        df_mean_ts = df.groupby(['period', 'category'])['product_lifetime'].mean(numeric_only=False)
 
-        df_mean_by_year = reduce(lambda df1,df2: pd.merge(df1,df2, on='category', how='outer'), dfs).transpose()
-        plot = df_mean_by_year.plot.line()
+        plot = df_mean_ts.unstack().plot.line()
         plot.plot()
         plt.show()
-        
-        return dfs
 
-# dfs = CategoryLifetime.render(df)
+# CategoryLifetime.render(df)
