aed611cfbcf6d35c02a0c6ab0fc7119d8f658370
J.M. Thiessen <jacob@x0rz3q.com>
2020-09-25 21:40:34 +0200

    Use the average based on the lifetime of the market

--- src/market_dominance.py
+++ src/market_dominance.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from qbstyles import mpl_style
+import pandas as pd
 from category_over_time import CategoryOverTime
+import numpy as np
 
 mpl_style(dark=False)
 
@@ -9,14 +11,25 @@ class MarketDominance:
     def render(df, split=False):
         for k,v in CategoryOverTime.mapping.items():
             df['category'] = df['category'].replace(k, v)
+        
+        # calculate the lifetime of the shop
+        df['first_month'] = pd.DatetimeIndex(df['first_observed']).to_period('M')
+        df['last_month'] = pd.DatetimeIndex(df['last_observed']).to_period('M')
+        first_seen = df[['marketplace', 'first_month']].groupby(['marketplace']).min().unstack()['first_month']
+        last_seen  = df[['marketplace', 'last_month']].groupby(['marketplace']).max().unstack()['last_month']
 
         df = df.groupby(['category', 'marketplace']).sum()['order_amount_usd'].unstack()
         df = df.fillna(0)
 
+        # calculate the average per month for a given market
+        for market in df:
+            lifetime = (last_seen[market] - first_seen[market]).n
+            df[market] = df[market] / lifetime
+
         if not split:
             ax = df.plot.bar(title='Category Revenue per Market', width=0.8)
             ax.set_xlabel('Category')
-            ax.set_ylabel('Revenue in USD')
+            ax.set_ylabel('Average revenue per month in USD')
             ax.legend(title='Marketplace')
             ax.plot()
             plt.tight_layout()
@@ -25,7 +38,7 @@ class MarketDominance:
             for market in df:
                 ax = df[market].plot.bar(title=f'Category Revenue of {market}', width=0.8)
                 ax.set_xlabel('Category')
-                ax.set_ylabel('Revenue in USD')
+                ax.set_ylabel('Average revenue per month in USD')
                 ax.plot()
                 plt.tight_layout()
                 plt.show()
